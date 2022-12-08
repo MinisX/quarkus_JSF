536ac3277caf25500bb2e44b892bcc3aef2fce64
Daniel Dedoukh <daniel.dedoukh@axians-ikvs.de>
2022-12-08 16:58:00 +0100

    JSF DateTimeConverter

--- jsf_tutorial_linkedin/src/main/java/page/HTML5PageBean.java
+++ jsf_tutorial_linkedin/src/main/java/page/HTML5PageBean.java
@@ -2,12 +2,15 @@ package page;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.util.Date;
 
 @Named("html5Bean")
 @RequestScoped
 public class HTML5PageBean {
 
     private String dateIn;
+
+    private Date dateObjectIn;
     private String dateOut;
 
     public void transformDates(){
@@ -28,5 +31,13 @@ public class HTML5PageBean {
 
     public void setDateOut(String dateOut) {
         this.dateOut = dateOut;
+    }
+
+    public Date getDateObjectIn() {
+        return dateObjectIn;
+    }
+
+    public void setDateObjectIn(Date dateObjectIn) {
+        this.dateObjectIn = dateObjectIn;
     }
 }
